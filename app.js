40e121bd9b048a22d4ca5dc1f7ac0bb3bdfb5760
Suzuna Arakaki <suzuna.arakaki@gmail.com>
2025-11-24 20:53:27 -0800

    Exercise is done

--- app.js
+++ app.js
@@ -1,3 +1,36 @@
 $(function() {
   // YOUR CODE HERE
+  $('#toggleButton').on('click', function() {
+    $(this).next('#myParagraph').slideToggle()
+  })
+
+  $('#colorButton').on('click', function() {
+    $('#colorDiv').css({ backgroundColor: "yellow"})
+  })
+
+  $('#addClassButton').on('click', function() {
+    $('#classDiv').addClass('new-class')
+    console.log("Added the new CSS class to the gray div")
+  })
+
+  $('#removeClassButton').on('click', function() {
+    $('#classDiv').removeClass()
+    console.log("Removed the CSS class from the gray div")
+  })
+
+  $('#fadeInButton').on('click', function() {
+    $('#fadeDiv').fadeIn( "slow", function() {})
+  })
+
+  $('#fadeOutButton').on('click', function() {
+    $('#fadeDiv').fadeOut( "slow", function() {})
+  })
+
+  $('#slideUpButton').on('click', function() {
+    $('#slideDiv').slideUp( "slow", function() {})
+  })
+
+  $('#slideDownButton').on('click', function() {
+    $('#slideDiv').slideDown( "slow", function() {})
+  })
 })
